0b079e14d354047403bcf2b01e980a1e432cf307
unknown <anagandula.soumya@gmail.com>
2023-08-31 20:31:00 +0530

    first commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,12 +3,20 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/TaskManager', { useNewUrlParser: true }).then(() => {
-    console.log("Connected to MongoDB successfully :)");
+// mongoose.connect('mongodb://127.0.0.1:27017/TaskManager', { useNewUrlParser: true }).then(() => {
+//     console.log("Connected to MongoDB successfully :)");
+// }).catch((e) => {
+//     console.log("Error while attempting to connect to MongoDB");
+//     console.log(e);
+// });
+
+mongoose.connect("mongodb+srv://anagandulasoumya:Soumya503@cluster0.uhsgdar.mongodb.net/TaskManager").then( () =>{
+    console.log("connected to MongoDB successfully :)");
 }).catch((e) => {
-    console.log("Error while attempting to connect to MongoDB");
-    console.log(e);
-});
+         console.log("Error while attempting to connect to MongoDB");
+         console.log(e);
+    
+})
 
 // To prevent deprectation warnings (from MongoDB native driver)
 //mongoose.set('useCreateIndex', true);
